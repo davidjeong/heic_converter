1575f51d13a8f938489c06dfa0758d5130da4fe6
David <djeong747@gmail.com>
2022-05-09 21:35:52 -0400

    Add script to convert heic to png

---
+++ converter.py
@@ -0,0 +1,40 @@
+import argparse
+import logging
+
+from wand.image import Image
+import os
+
+HEIC_SUFFIX = ".HEIC"
+PNG_SUFFIX = ".png"
+
+logging.basicConfig(
+    filename='output.log',
+    encoding='utf-8',
+    level=logging.INFO
+)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', help='Source directory', required=True)
+    parser.add_argument('-d', '--destination', help='Destination directory')
+    args = parser.parse_args()
+
+    destination = args.source if args.destination is None else args.destination
+
+    for file in os.listdir(args.source):
+        source_file = os.path.join(args.source, file)
+        destination_file = os.path.join(destination, file.replace(HEIC_SUFFIX, PNG_SUFFIX))
+
+        img = Image(filename=source_file)
+        img.format = 'png'
+        img.save(filename=destination_file)
+        img.close()
+
+        logging.info("Finished processing file %s", destination_file)
+
+    logging.info("Finished processing all files")
+
+
+if __name__ == "__main__":
+    main()
